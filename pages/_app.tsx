8334b078d6a829a79aedb959a30c354b6ddc8023
TabletopNonsenseverse <aboltabol.nonsenseverse@gmail.com>
2026-08-13 23:36:15 +0200

    Add playtest banner to the app component
    Added a playtest banner with a link to the playtest version of Astrovoyage.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+const PLAYTEST_URL = 'https://tabletopnonsenseverse.myshopify.com/products/astrovoyage'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="playtest-banner">
+        <span>Get the playtest version of Astrovoyage here.</span>
+        <a
+          href={PLAYTEST_URL}
+          target="_blank"
+          rel="noreferrer"
+          className="playtest-button"
+        >
+          Get the Playtest
+        </a>
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
